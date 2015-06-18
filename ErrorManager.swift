dace9e6f8307214d8612512fe123d1559a8fab5c
yu-ichiro <yu-ichiro@users.noreply.github.com>
2015-06-18 22:49:55 +0900

    [Fix] timestamp to be localized

--- ErrorManager.swift
+++ ErrorManager.swift
@@ -14,7 +14,7 @@ class ErrorManager {
 	
 	func invokeErrorWithErrorCode(e_code:Int, displayMessage dm:Bool, viewControllerToDisplayAlert vc:UIViewController?) {
 		let id = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleIdentifier") as! String;
-		print("\(NSDate().description) \(id) \(e_code) \(self.localizedMessageForErrorCode(e_code))");
+		print("\(NSDate().descriptionWithLocale(NSLocale.currentLocale())) \(id) \(e_code) \(self.localizedMessageForErrorCode(e_code))");
 		if dm {
 			let ac = UIAlertController(title: "Error", message: self.localizedMessageForErrorCode(e_code), preferredStyle: UIAlertControllerStyle.Alert);
 			ac.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil));
